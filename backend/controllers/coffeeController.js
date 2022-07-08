5573abf89f1a1e92aa33e3b94df2739d4886bcc5
David <nizz3hx@protonmail.com>
2022-07-08 17:40:55 +0100

    updates

--- backend/controllers/coffeeController.js
+++ backend/controllers/coffeeController.js
@@ -1,29 +1,38 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc GET Coffee
 // @route GET /api/coffee
 // @access Private
-const getCoffee = (req, res) => {
+const getCoffee = asyncHandler((req, res) => {
+
+    
     res.status(200).json({ message: 'Get Coffee'})
-}
+})
 // @desc ADD Coffee
 // @route POST /api/coffee/
 // @access Private
-const addCoffee = (req, res) => {
+const addCoffee = asyncHandler(async (req, res) => {
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
+
     res.status(200).json({ message: "Set coffee" });
-}
+})
 
 // @desc GET Coffee
 // @route PUT /api/coffee/:id
 // @access Private
-const editCoffee = (req, res) => {
+const editCoffee = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update coffee ${req.params.id}` });
-}
+})
 
-// @desc GET Coffee
+// @desc DELETE Coffee
 // @route DELETE /api/coffee/:id
 // @access Private
-const deleteCoffee = (req, res) => {
+const deleteCoffee = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete coffee ${req.params.id}` });
-}
+})
 
  module.exports = {
     getCoffee,
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+
+connectDB()
 
 const app = express()
 
+//Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 // Routes
 app.use('/api/coffee', require('./routes/coffeeRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on ${port}`))
